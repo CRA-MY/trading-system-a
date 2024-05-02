1a9db38bea019fb95005455479cb6dbda0b2e0ec
jungmin <gmlsajfl93@gmail.com>
2024-05-02 12:36:01 +0900

    [feature] Driver 1차 개발

--- src/main/java/driver/StockerBrockerDriverKiwer.java
+++ src/main/java/driver/StockerBrockerDriverKiwer.java
@@ -1,4 +1,31 @@
 package driver;
 
-public class StockerBrockerDriverKiwer {
+import api.KiwerAPI;
+
+public class StockerBrockerDriverKiwer implements IStockerBrockerDriver {
+    KiwerAPI api;
+
+    public StockerBrockerDriverKiwer() {
+        this.api = new KiwerAPI();
+    }
+
+    public StockerBrockerDriverKiwer(KiwerAPI kiwer) {
+        this.api = kiwer;
+    }
+
+    public void login(String ID, String Password) {
+        api.login(ID, Password);
+    }
+
+    public void buy( String stockCode, int count , int price) {
+        api.buy(stockCode, count, price);
+    }
+
+    public void sell( String stockCode , int count , int price) {
+        api.sell(stockCode, count, price);
+    }
+
+    public int currentPrice ( String stockCode) {
+        return api.currentPrice(stockCode);
+    }
 }
--- src/main/java/driver/StockerBrockerDriverNemo.java
+++ src/main/java/driver/StockerBrockerDriverNemo.java
@@ -1,4 +1,27 @@
 package driver;
 
-public class StockerBrockerDriverNemo {
+import api.NemoAPI;
+
+public class StockerBrockerDriverNemo implements IStockerBrockerDriver {
+    NemoAPI api;
+
+    public StockerBrockerDriverNemo() {
+        this.api = new NemoAPI();
+    }
+
+    public void login( String ID, String Password) {
+        api.certification(ID, Password);
+    }
+
+    public void buy( String stockCode, int count , int price) {
+        api.purchasingStock(stockCode, count, price);
+    }
+
+    public void sell( String stockCode , int count , int price) {
+        api.sellingStock(stockCode, count, price);
+    }
+
+    public int currentPrice ( String stockCode) throws InterruptedException {
+        return api.getMarketPrice(stockCode, 0);
+    }
 }
