c58b6162c7ddd2813f37f083441b7451b23143bf
dw.koh <dw0402.koh@samsung.com>
2024-05-02 12:44:33 +0900

    3. 기본기능을 가진 Application (고동완님) 개발건입니다.

--- src/main/java/TradingApplication.java
+++ src/main/java/TradingApplication.java
@@ -1,2 +1,47 @@
+
+class DriverNotFoundException extends RuntimeException {
+    public DriverNotFoundException(String msg) {
+        super(msg);
+    }
+}
+
 public class TradingApplication {
+    StockerBrockerDriver driver;
+
+    public TradingApplication() {
+
+    }
+
+    public TradingApplication(StockerBrockerDriver driver) {
+        this.driver = driver;
+    }
+
+    public void checkDriver() {
+        if(driver == null)
+            throw new DriverNotFoundException("DRIVER NOT FOUND");
+    }
+
+    public void selectStockBrocker(StockerBrockerDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String id, String pwd) {
+        checkDriver();
+        driver.login(id, pwd);
+    }
+
+    public void buy(String stockCode, int price, int count) {
+        checkDriver();
+        driver.buy(stockCode, price, count);
+    }
+
+    public void sell(String stockCode, int price, int count) {
+        checkDriver();
+        driver.sell(stockCode, price, count);
+    }
+
+    public int currentPrice(String stockCode) {
+        checkDriver();
+        return driver.getPrice(stockCode);
+    }
 }
