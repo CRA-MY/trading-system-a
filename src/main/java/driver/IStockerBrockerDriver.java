bb7a2d236bcd40ac5b4883c7dda03739ded1ae93
metrics21 <dnflskfk8665@>
2024-05-02 12:36:06 +0900

    interface2-2

--- src/main/java/driver/IStockerBrockerDriver.java
+++ src/main/java/driver/IStockerBrockerDriver.java
@@ -2,15 +2,15 @@ package driver;
 
 public interface IStockerBrockerDriver {
     // 로그인
-    void login();
+    void login(String ID, String Password);
 
     // 구매
-    void buy();
+    void buy(String stockCode, int count , int price);
 
     // 판매
-    void sell();
+    void sell(String stockCode , int count , int price);
 
     // 가격확인
-    int getPrice();
+    int getPrice(String stockCode);
 
 }
