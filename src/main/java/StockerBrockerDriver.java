36849ba5be5a4e45e2eaf72c8fa466010b5930ce
metrics21 <dnflskfk8665@>
2024-05-02 10:49:47 +0900

    nemo interface

--- src/main/java/StockerBrockerDriver.java
+++ src/main/java/StockerBrockerDriver.java
@@ -1,14 +1,18 @@
 public interface StockerBrockerDriver {
     // 로그인
     void login();
+    void certification();
 
     // 구매
     void buy();
+    void purchasingStock();
 
     // 판매
     void sell();
+    void sellingStock();
 
-    // 가격확인
+            // 가격확인
     int getPrice();
+    int getMarketPrice ();
 
 }
